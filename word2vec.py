4eaa7e703ecb3e7d97b83b8390ce89ac17acb28c
Terry Cox <terry@bootstrap.je>
2019-04-01 14:49:45 +0100

    fix: Allocate 3Gb RAM

--- word2vec.py
+++ word2vec.py
@@ -302,50 +302,13 @@ def word2vec_basic(log_dir):
 
     writer.close()
 
-    # Step 6: Visualize the embeddings.
-
-    # pylint: disable=missing-docstring
-    # Function to draw visualization of distance between embeddings.
-    def plot_with_labels(low_dim_embs, labels, filename):
-        assert low_dim_embs.shape[0] >= len(
-            labels), 'More labels than embeddings'
-        plt.figure(figsize=(18, 18))  # in inches
-        for i, label in enumerate(labels):
-            x, y = low_dim_embs[i, :]
-            plt.scatter(x, y)
-            plt.annotate(
-                label,
-                xy=(x, y),
-                xytext=(5, 2),
-                textcoords='offset points',
-                ha='right',
-                va='bottom')
-
-        plt.savefig(filename)
-
-    try:
-        # pylint: disable=g-import-not-at-top
-        from sklearn.manifold import TSNE
-        import matplotlib.pyplot as plt
-
-        tsne = TSNE(
-            perplexity=30, n_components=2, init='pca', n_iter=5000, method='exact')
-        plot_only = 500
-        low_dim_embs = tsne.fit_transform(final_embeddings[:plot_only, :])
-        labels = [reverse_dictionary[i] for i in xrange(plot_only)]
-        plot_with_labels(low_dim_embs, labels, os.path.join(gettempdir(),
-                                                            'tsne.png'))
-
-    except ImportError as ex:
-        print('Please install sklearn, matplotlib, and scipy to show embeddings.')
-        print(ex)
-
 
 # All functionality is run after tf.app.run() (b/122547914). This could be split
 # up but the methods are laid sequentially with their usage for clarity.
 def main(unused_argv):
     # Give a folder path as an argument with '--log_dir' to save
     # TensorBoard summaries. Default is a log folder in current directory.
+    
     current_path = os.path.dirname(os.path.realpath(sys.argv[0]))
 
     parser = argparse.ArgumentParser()
